82f9a6c70746c4fd4e444d1a00365a9a0cf14f6c
Daniel Gutierrez Muñoz <dagutmu667@gmail.com>
2023-08-09 10:54:25 -0500

    chore: set up TS, ESLint, and Prettier

---
+++ .eslintrc.cjs
@@ -0,0 +1,19 @@
+/** @type {import("eslint").Linter.Config} */
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: true,
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended-type-checked',
+    'plugin:@typescript-eslint/stylistic-type-checked',
+    'prettier',
+  ],
+};
---
+++ .prettierrc.cjs
@@ -0,0 +1,9 @@
+/** @type {import("prettier").Config} */
+module.exports = {
+  semi: true,
+  tabWidth: 2,
+  singleQuote: true,
+  trailingComma: 'es5',
+  printWidth: 70,
+  endOfLine: 'crlf',
+};
